3f68bf1fee7b338a909933b10f90a8a5126467bc
anshul singh <singhanshul2807@gmail.com>
2021-05-10 14:17:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const result = document.querySelector("#result").innerHTML;
+
+
+const len=2,ul=0,ll=0,num=0,sym=0;
+
+const defaults = [15,0,0,0,0];
+defaults[0]=document.getElementById('len').value;
+console.log(defaults[0]);
+
+
+
+
